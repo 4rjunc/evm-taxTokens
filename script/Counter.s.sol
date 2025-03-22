a7f2ec68a1e537fb21d96341191e08de119c92a7
4rjunc <arjunmundamani@gmail.com>
2025-03-22 22:43:01 +0530

    first commit

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract CounterScript  {
 
-    function setUp() public {}
-
-    function run() public {
-        vm.startBroadcast();
-
-        counter = new Counter();
-
-        vm.stopBroadcast();
-    }
 }
