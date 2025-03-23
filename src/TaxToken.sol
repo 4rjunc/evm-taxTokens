ec9b35e7456adff6a723ab49438c746acdcd72f6
4rjunc <arjunmundamani@gmail.com>
2025-03-23 11:45:11 +0530

    Comments

--- src/TaxToken.sol
+++ src/TaxToken.sol
@@ -36,7 +36,7 @@ contract TaxToken is ERC20 {
       // hypothetically the demands stays the same price goes up
 
       _transfer(msg.sender, to, transferAmount);
-      _transfer(msg.sender, address(0), taxAmount);
+      _transfer(msg.sender, address(0), taxAmount); // can change the address with our wallet address
 
       emit Transfer(msg.sender, to, amount);
 
